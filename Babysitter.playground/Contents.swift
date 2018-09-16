e54bef603bbcad78868e8dde409727509c4534c3
Archon Light <mike@altri.us>
2018-09-16 17:31:51 -0400

    Functions added to Playground. Now a string date is converted to a Date object

--- Babysitter.playground/Contents.swift
+++ Babysitter.playground/Contents.swift
@@ -16,8 +16,31 @@
 import UIKit
 
 struct Babysitter {
+    //setup constants for hourly wages
+    let startToBedTimeWage: Float = 12.0
+    let bedtimeToMidnightWage: Float = 8.0
+    let midnightToEndOfJobWage : Float = 16.0
+    
+    //calculate the nightly charge for hours worked
+    func calculateNightlyCharge(startTime: Date, bedTime: Date, midnightTime: Date, endTime: Date) -> Float {
+        return 0.0
+    }
+    
+    //convert the Date from String to Date
+    func convertDateStringToDate(dateString: String) -> Date{
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yy-MM-dd HH:mm:ss Z"
+        dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        return dateFormatter.date(from: dateString)!
+    }
     
 }
 
 
 let babysitter = Babysitter()
+//setup time constants for the hours worked. -0400 sets to our timezone
+let startTime = babysitter.convertDateStringToDate(dateString: "2018-09-16 17:00:00 -0400")
+let bedTime = babysitter.convertDateStringToDate(dateString: "2018-09-16 21:00:00 -0400")
+let midnightTime = babysitter.convertDateStringToDate(dateString: "2018-09-17 00:00:00 -0400")
+let endTime = babysitter.convertDateStringToDate(dateString: "2018-09-17 00:00:00 -0400")
